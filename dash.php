6c802025e6e3a9abac3bdd8991b766e877d8b4cb
Adhim Nioka <niokagi@gmail.com>
2023-06-20 15:14:12 +0700

    ok

--- dash.php
+++ dash.php
@@ -22,18 +22,24 @@ $result_menu = $conn->query($sql_menu);
 
 <div class="container" style="background-image: url(img/r1.jpg)">
     <div>
-        <h1>Welcome <?= $_SESSION["username"] ?>!</h1>
+        <h1>Welcome
+            <?= $_SESSION["username"] ?>!
+        </h1>
         <p>Savor the culinary experience of Nioka Brasserie,<br>where every dish is a delight.</p>
         <a href="#main_course"><button>See More</button></a>
     </div>
 </div>
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-    <path fill="#000000" fill-opacity="1" d="M0,192L30,186.7C60,181,120,171,180,170.7C240,171,300,181,360,186.7C420,192,480,192,540,170.7C600,149,660,107,720,117.3C780,128,840,192,900,213.3C960,235,1020,213,1080,181.3C1140,149,1200,107,1260,96C1320,85,1380,107,1410,117.3L1440,128L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path>
+    <path fill="#000000" fill-opacity="1" d="M0,192L30,186.7C60,181,120,171,180,170.7C240,171,300,181,360,186.7C420,192,480,192,540,170.7C600,149,660,107,720,117.3C780,128,840,192,900,213.3C960,235,1020,213,1080,181.3C1140,149,1200,107,1260,96C1320,85,1380,107,1410,117.3L1440,128L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z">
+    </path>
 </svg>
 <div class="content">
     <div class="hero" id="main_course">
         <h2>Maincourse</h2>
-        <p>Features a selection of our most sought-after and beloved dishes. From perfectly grilled premium steaks to tantalizingly sweet desserts, each item in our Poppulars menu is crafted to deliver an unforgettable culinary experience. Every bite will indulge your taste buds and satisfy your cravings for exceptional flavors.</p>
+        <p>Features a selection of our most sought-after and beloved dishes. From perfectly grilled premium steaks to
+        tantalizingly sweet desserts, each item in our Poppulars menu is crafted to deliver an unforgettable
+        culinary experience. Every bite will indulge your taste buds and satisfy your cravings for exceptional
+        flavors.</p>
     </div>
 </div>
 <div class="blog">
@@ -73,7 +79,10 @@ $result_menu = $conn->query($sql_menu);
 <div class="content">
     <div class="hero" id="dessert">
         <h2>Dessert</h2>
-        <p>Features a selection of our most sought-after and beloved dishes. From perfectly grilled premium steaks to tantalizingly sweet desserts, each item in our Poppulars menu is crafted to deliver an unforgettable culinary experience. Every bite will indulge your taste buds and satisfy your cravings for exceptional flavors.</p>
+        <p>Features a selection of our most sought-after and beloved dishes. From perfectly grilled premium steaks to
+            tantalizingly sweet desserts, each item in our Poppulars menu is crafted to deliver an unforgettable
+            culinary experience. Every bite will indulge your taste buds and satisfy your cravings for exceptional
+            flavors.</p>
     </div>
 </div>
 <div class="blog">
@@ -105,10 +114,10 @@ $result_menu = $conn->query($sql_menu);
 <script>
     window.history.pushState(null, null, window.location.href);
     window.onpopstate = function() {
-        window.history.go(1);
+    window.history.go(1);
     };
 </script>
 
 <?php
-include "footer.php";
+    include "footer.php";
 ?>
